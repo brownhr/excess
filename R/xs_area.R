b0bf5bd09751c83bd3c44e8f43a1c2a7cc9c038f
brownhr <brownhr314@gmail.com>
2022-02-18 09:11:34 -0500

    Removed depreciated functions

--- R/xs_area.R
+++ R/xs_area.R
@@ -1,65 +1,27 @@
-#' Calculate XS area from data
+#' Calculate Cross-sectional area
 #' @param .data A data.frame of cross-section data
-#' @return A Double of the cross-sectional area compared to a baseline (default, *Bankful*)
+#' @param .x_cor A column of x-coordinates (default, *TAPE*)
+#' @param .invertrod A column of rod readings, relative to 0 (i.e. >0) (default, *InvertRod*)
+#' @param .bankfull A column of bankfull readings, used as a baseline (default, *Bankful*)
+#' @return A Double of the cross-sectional area compared to a baseline
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
 #' @import dplyr
 #' @export
 
-
-
-
-# # Function is Depreciated... remove later
-# xs_area2 <- function(.data) {
-#   # Mutate the InvertRod to be relative to Bankful;
-#   # This can easily be appended to also calculate area relative to another
-#   # feature, such as WS or whatever else.
-#
-#
-#   # Set values of InvertRod > Bankful equal to Bankful, then normalize the
-#   # whole dataset such that Bankful = 0 (baseline)
-#
-#   .data <- .data %>%
-#     dplyr::mutate(Depth_Baseline = (pmin(.data$InvertRod, .data$Bankful) - .data$Bankful))
-#
-#
-#   # Approximate integration using trapezoidal rule with basepoints at x = xs$TAPE
-#   area <- abs(pracma::trapz(x = .data$TAPE,
-#                             y = .data$Depth_Baseline))
-#   return(area)
-# }
-
-
 xs_area <- function(.data,
-                     .x_cor = TAPE,
-                     .invertrod = InvertRod,
-                     .bankfull = Bankful) {
+                    .x_cor = TAPE,
+                    .invertrod = InvertRod,
+                    .bankfull = Bankful) {
   .x_cor <- enquo(.x_cor)
   .invertrod <- enquo(.invertrod)
   .bankfull <- enquo(.bankfull)
 
   .data %>%
-    mutate(depth_bf = pmin(!!.bankfull, !!.invertrod)-!!.bankfull) %>%
-    summarize(area = abs(pracma::trapz(x = !!.x_cor,
-                                       y = .data$depth_bf))) %>%
+    mutate(depth_bf = pmin(!!.bankfull,!!.invertrod)-!!.bankfull) %>%
+    summarize(area = abs(pracma::trapz(
+      x = !!.x_cor,
+      y = .data$depth_bf
+    ))) %>%
     as.double()
 }
-
-
-
-# path <- "data"
-
-# xs_files <- (list.files(path, pattern = ".xlsx", full.names = T))
-
-# xs_areas <- sapply(
-#   X = xs_files,
-#   FUN = function(x) {
-#     file = read_xlsx(x)
-#     area = xs_area(file)
-#     return(area)
-#   }
-# ) %>%
-#   # Convert to a df; code after this is unnecessary if you just want a named vector
-#   as.data.frame() %>%
-#   tibble::rownames_to_column(var = "xs_Name") %>%
-#   dplyr::rename(xs_Area = ".")
