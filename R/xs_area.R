5219b528ebf600aa61184e6edf0120c24a6df9bb
brownhr <brownhr314@gmail.com>
2022-02-17 20:47:18 -0500

    First Build
    xs_area and xs_ribbon

--- R/xs_area.R
+++ R/xs_area.R
@@ -1,19 +1,44 @@
+#' Calculate XS area from data
+#' @param .data A data.frame of cross-section data
+#' @return A Double of the cross-sectional area compared to a baseline (default, *Bankful*)
+#' @importFrom magrittr %>%
+#' @importFrom rlang .data
+#' @import dplyr
 
-xs_area <- function(xs) {
+
+
+xs_area <- function(.data) {
   # Mutate the InvertRod to be relative to Bankful;
   # This can easily be appended to also calculate area relative to another
   # feature, such as WS or whatever else.
-  
-  
+
+
   # Set values of InvertRod > Bankful equal to Bankful, then normalize the
   # whole dataset such that Bankful = 0 (baseline)
-  
-  xs <- xs %>%
-    dplyr::mutate(InvertRod_Bankful = (pmin(InvertRod, Bankful) - Bankful))
-  
-  
+
+  .data <- .data %>%
+    dplyr::mutate(Depth_Baseline = (pmin(.data$InvertRod, .data$Bankful) - .data$Bankful))
+
+
   # Approximate integration using trapezoidal rule with basepoints at x = xs$TAPE
-  area <- abs(pracma::trapz(x = xs$TAPE,
-                            y = xs$InvertRod_Bankful))
+  area <- abs(pracma::trapz(x = .data$TAPE,
+                            y = .data$Depth_Baseline))
   return(area)
 }
+
+# path <- "data"
+
+# xs_files <- (list.files(path, pattern = ".xlsx", full.names = T))
+
+# xs_areas <- sapply(
+#   X = xs_files,
+#   FUN = function(x) {
+#     file = read_xlsx(x)
+#     area = xs_area(file)
+#     return(area)
+#   }
+# ) %>%
+#   # Convert to a df; code after this is unnecessary if you just want a named vector
+#   as.data.frame() %>%
+#   tibble::rownames_to_column(var = "xs_Name") %>%
+#   dplyr::rename(xs_Area = ".")
--- R/xs_ribbon.R
+++ R/xs_ribbon.R
@@ -1,17 +1,27 @@
+#' Generate an area plot in ggplot2
+#' @param .data Cross section data
+#' @return A ggplot
+#' @import ggplot2
+#' @importFrom rlang .data
+
+
 # Tentative way of plotting the area lower than Bankful. It's a bit inaccurate if
 # there isn't an observation at InvertRod == Bankful
-require(ggplot2)
 
-xs_ribbon <- function(data){
-ggplot(data = data, aes(x = TAPE, y = InvertRod)) +
-  geom_path() +
-  geom_path(aes(x = TAPE, y = Bankful)) + geom_ribbon(aes(
-    x = TAPE,
-    ymax = Bankful,
-    ymin = pmin(Bankful, InvertRod)
-  ),
-  fill = "blue",
-  alpha = 0.5)
+xs_ribbon <- function(.data) {
+  gg <-
+    ggplot(data = .data, aes(x = .data$TAPE, y = .data$InvertRod)) +
+    geom_path() +
+    geom_path(aes(x = .data$TAPE, y = .data$Bankful)) + geom_ribbon(
+      aes(
+        x = .data$TAPE,
+        ymax = .data$Bankful,
+        ymin = pmin(.data$Bankful, .data$InvertRod)
+      ),
+      fill = "blue",
+      alpha = 0.5
+    )
+  return(gg)
 }
 
 # cowplot::plot_grid(plotlist = xs_gg, labels = rownames(xs_areas))
