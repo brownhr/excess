8d137d4632507d73b2b50b49a983fc6cec6046c1
brownhr <brownhr314@gmail.com>
2022-02-21 14:46:23 -0500

    Renamed variables in `xs_ribbon`

--- R/xs_ribbon.R
+++ R/xs_ribbon.R
@@ -9,16 +9,20 @@
 # Tentative way of plotting the area lower than Bankful.
 # It's a bit inaccurate if there isn't an observation at InvertRod == Bankful
 
-xs_ribbon <- function(.data) {
+xs_ribbon <- function(.data,
+                      x_cor = "TAPE",
+                      depth = "InvertRod",
+                      baseline = "Bankful",
+                      ...) {
   gg <-
-    ggplot(data = .data, aes(x = .data$TAPE, y = .data$InvertRod)) +
+    ggplot(data = .data, aes(x = .data[[x_cor]], y = .data[[depth]])) +
     geom_path() +
-    geom_path(aes(x = .data$TAPE, y = .data$Bankful)) +
+    geom_path(aes(x = .data[[x_cor]], y = .data[[baseline]])) +
     geom_ribbon(
       aes(
-        x = .data$TAPE,
-        ymax = .data$Bankful,
-        ymin = pmin(.data$Bankful, .data$InvertRod)
+        x = .data[[x_cor]],
+        ymax = .data[[baseline]],
+        ymin = pmin(.data[[baseline]], .data[[depth]])
       ),
       fill = "blue",
       alpha = 0.5
