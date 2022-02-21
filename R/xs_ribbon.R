913886491b013a47bdc0cfab2bb04a06b91e571d
brownhr <brownhr314@gmail.com>
2022-02-21 14:51:50 -0500

    Update `xs_ribbon` documentation

--- R/xs_ribbon.R
+++ R/xs_ribbon.R
@@ -1,6 +1,11 @@
 #' Generate an area plot in ggplot2
-#' @param .data Cross section data
-#' @return A ggplot
+#' @param .data A data.frame of cross-section data
+#' @param x_cor A column of x-coordinates (default, *TAPE*)
+#' @param depth A column of rod readings, relative to 0 (i.e. >0) (default,
+#'   *InvertRod*)
+#' @param baseline A column of baseline readings (default, *Bankful*)
+#' @param ... Other arguments passed on to methods.
+#' @return An object of class `ggplot`
 #' @import ggplot2
 #' @importFrom rlang .data
 #' @export
