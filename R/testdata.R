83236475a2d598b0e01e89a68b44d99a6afe1425
brownhr <brownhr314@gmail.com>
2022-02-21 12:14:50 -0500

    Renamed data.R to testdata.R

--- R/testdata.R
+++ R/testdata.R
@@ -1,8 +1,13 @@
 #' Sample cross-sectional data
+#'
 #' Cross-sectional data acquired from the Watauga River, Boone, N.C., U.S.
 #'
 #' @author Josh Platt
 #' @author Quincy Williams
+#'
+#' @docType data
+#'
+#'
 #'
 #' @format A tibble of 34 rows and 3 variables:
 #' \describe{
