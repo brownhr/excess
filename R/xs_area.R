c79e13af162c7655f405278535629b0a8fa0df50
brownhr <brownhr314@gmail.com>
2022-02-21 15:01:09 -0500

    Explicit `dplyr::fun()` calls in `xs_area`

--- R/xs_area.R
+++ R/xs_area.R
@@ -6,25 +6,24 @@
 #'   measurement.
 #' @param .data A data.frame of cross-section data
 #' @param x_cor A column of x-coordinates (default, *TAPE*)
-#' @param depth A column of rod readings, relative to 0 (i.e. >0)
-#' @param baseline A column of baseline readings, defaults to Bankful
+#' @param depth A column of rod readings, relative to 0 (i.e. >0) (default,
+#'   *InvertRod*)
+#' @param baseline A column of baseline readings (default, *Bankful*)
 #' @return A Double of the cross-sectional area compared to a baseline
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
-#' @import dplyr
+#' @importFrom dplyr summarize mutate
 #' @export
 
 xs_area <- function(.data,
                     x_cor = "TAPE",
                     depth = "InvertRod",
                     baseline = "Bankful") {
-  # x_cor <- enquo(x_cor)
-  # invertrod <- enquo(invertrod)
-  # baseline <- enquo(baseline)
-
   .data %>%
-    mutate(depth_baseline = pmin(.data[[baseline]], .data[[depth]]) - .data[[baseline]]) %>%
-    summarize(area = abs(pracma::trapz(
+    dplyr::mutate(
+      depth_baseline =
+        pmin(.data[[baseline]], .data[[depth]]) - .data[[baseline]]) %>%
+    dplyr::summarize(area = abs(pracma::trapz(
       x = .data[[x_cor]],
       y = .data$depth_baseline
     ))) %>%
