22a2e60997bea01703dfacd683445f49756a0542
brownhr <brownhr314@gmail.com>
2022-02-21 11:49:02 -0500

    remove tidyeval

--- R/data.R
+++ R/data.R
@@ -1,9 +1,13 @@
-#' @title Sample cross-sectional data
-#' @description Cross-sectional data acquired from the Watauga River, Boone, N.C., U.S.
+#' Sample cross-sectional data
+#' Cross-sectional data acquired from the Watauga River, Boone, N.C., U.S.
 #'
 #' @author Josh Platt
 #' @author Quincy Williams
 #'
-
-
+#' @format A tibble of 34 rows and 3 variables:
+#' \describe{
+#'   \item{TAPE}{Tape measurement; x-coordinate}
+#'   \item{InvertRod}{Depth measurement in meters}
+#'   \item{Bankful}{Bankful measurement}
+#' }
 "testdata"
