a6dde40ef4a4304acf21f183a22bfca27955aad2
brownhr <brownhr314@gmail.com>
2022-03-01 13:46:22 -0500

    Renamed `testdata.R` to `data.R`

--- R/data.R
+++ R/data.R
@@ -16,3 +16,19 @@
 #'   \item{Bankful}{Bankful measurement}
 #' }
 "testdata"
+
+
+#' @title Sample pebble count data 1
+#'
+#' @description Pebble count data acquired from the Downstream-2 Cross Section 2
+#'   site; Sep. 13, 2021
+#'
+#' @author Josh Platt
+#' @docType data
+#'
+#' @usage NULL
+#' @format A matrix of 20 rows and 8 columns: each column represents a transect
+#'   and each cell in that column represents a recording from river left (row 1)
+#'   to river right (row 20)
+#'
+"pebble1"
