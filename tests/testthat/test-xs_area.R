429610cfe8da1c909d34fee4722e5e8dbbe3f4ee
brownhr <brownhr314@gmail.com>
2022-06-02 01:44:18 -0400

    Update test-xs_area.R

--- tests/testthat/test-xs_area.R
+++ tests/testthat/test-xs_area.R
@@ -1,3 +1,11 @@
 test_that("multiplication works", {
-  expect_equal(units::set_units(xs_area(testxs), NULL), 23.4665)
+  expect_equal(object = as.numeric(xs_area(testxs)),-23.4665)
+})
+
+test_that("Units in testxs carry through xs-area", {
+  expect_equal(
+    object = units::as_units(units(testxs$TAPE)),
+    expected = units::as_units("ft"),
+    ignore_attr = TRUE
+  )
 })
