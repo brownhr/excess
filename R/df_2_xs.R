347d27d667afd6e4bf337c36b189e95d19875e33
brownhr <brownhr314@gmail.com>
2022-06-06 16:49:25 -0400

    Update df_2_xs.R

--- R/df_2_xs.R
+++ R/df_2_xs.R
@@ -1,28 +1,34 @@
-new_xs <- function(x, units, tape, depth, baseline) {
-  x <- structure(x,
-    class = "xs",
-    Units = units,
-    tape = tape,
-    depth = depth,
-    baseline = baseline
-  )
+new_xs <- function(x, tape, depth, baseline) {
+  x <- structure(x, class = append(class(x), "xs"),
+                 tape = tape,
+                 depth = depth,
+                 baseline = baseline)
+
+
   x
 }
 
 
-xs <- function(obj, ...) {
-  UseMethod("xs", obj)
+xs <- function(x, ...) {
+  UseMethod("xs", x)
 }
 
-xs.default <- function(obj, ...){
-  new_xs(x = obj, ...)
+xs.default <- function(x, ...){
+  new_xs(x = x, ...)
 }
 
 xs.data.frame <- function(xs,
                           x_col,
                           d_col,
-                          baseline,
-                          units = NULL) {
-
-
+                          baseline) {
+  x_col <- deparse(substitute(x_col))
+  d_col <- deparse(substitute(d_col))
+  baseline <- deparse(substitute(basline))
+  xs <- new_xs(
+    xs,
+    tape = x_col,
+    depth = d_col,
+    baseline = baseline
+  )
+  return(xs)
 }
