228fd2a3c6b2459813009024e8445b27ea17ffa2
brownhr <brownhr314@gmail.com>
2022-06-06 18:14:21 -0400

    Update df_2_xs.R

--- R/df_2_xs.R
+++ R/df_2_xs.R
@@ -1,19 +1,33 @@
+#' @title Constructor for class \code{xs}
+#' @description Basic dev-level constructor for generating a cross-section object ('xs')
+#' @param x Object of class \code{data.frame}
+#' @param tape Column of x from which the cross-coordinates (tape) are to be read
+#' @param depth Column of x from which the rod measurements (depth) are to be read
+#' @param baseline Column of x from which the bankful values (baseline) are to be read
+#' @return An object of class \code{xs}
+
+#' @seealso
+#'  \code{\link[purrr]{prepend}}
+#'  \code{\link[dplyr]{pull}}
+#' @rdname new_xs
 #' @export
+#' @importFrom purrr prepend
+#' @importFrom dplyr pull
 new_xs <- function(x, tape, depth, baseline) {
-  x <- structure(x, class = purrr::prepend(class(x), "xs"),
-                 tape = dplyr::pull(x, {{tape}}),
-                 depth = dplyr::pull(x, {{depth}}),
-                 baseline = dplyr::pull(x, {{baseline}}))
-  stopifnot(
-    all(
-      is.numeric(attr(xs_testxs, "tape")),
-      is.numeric(attr(xs_testxs, "depth")),
-      is.numeric(attr(xs_testxs, "baseline"))
+  x <- structure(
+    x,
+    class = purrr::prepend(class(x), "xs"),
+    tape = dplyr::pull(x, {{tape}}),
+    depth = dplyr::pull(x, {{depth}}),
+    baseline = dplyr::pull(x, {{baseline}}))
 
-    )
-  )
+  stopifnot(all(is.numeric(attr(xs_testxs, "tape")),
+                is.numeric(attr(xs_testxs, "depth")),
+                is.numeric(attr(
+                  xs_testxs, "baseline"
+                ))))
 
-  x
+  return(x)
 }
 
 #
