32d24ad3c38aa2113fd1bbfd114543cab871fac0
brownhr <brownhr314@gmail.com>
2022-06-06 17:32:25 -0400

    Added additional files

--- data-raw/testxs.R
+++ data-raw/testxs.R
@@ -1,8 +1,12 @@
 ## code to prepare `testdata` dataset goes here
-library(dplyr)
+library(tidyverse)
 testxs <- readr::read_csv("data-raw/testdata.csv")
 testxs <- testxs %>% mutate(across(everything(), set_units, "ft"))
 
 testxs$ID <- c(letters, LETTERS)[1:nrow(testxs)]
 
 usethis::use_data(testxs, overwrite = TRUE)
+
+testxs2 <- readxl::read_xlsx("data-raw/xs-ds1/DS1XS3_1.xlsx")
+
+usethis::use_data(testxs2, overwrite = TRUE)
