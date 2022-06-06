7e8a24d9f9cf6d5af9970882cde38ba6770820ef
brownhr <brownhr314@gmail.com>
2022-06-06 17:32:56 -0400

    Got S3 methods for class `xs` (mostly) working!

--- R/df_2_xs.R
+++ R/df_2_xs.R
@@ -1,34 +1,42 @@
+#' @export
 new_xs <- function(x, tape, depth, baseline) {
-  x <- structure(x, class = append(class(x), "xs"),
-                 tape = tape,
-                 depth = depth,
-                 baseline = baseline)
+  x <- structure(x, class = purrr::prepend(class(x), "xs"),
+                 tape = dplyr::pull(x, {{tape}}),
+                 depth = dplyr::pull(x, {{depth}}),
+                 baseline = dplyr::pull(x, {{baseline}}))
+  stopifnot(
+    all(
+      is.numeric(attr(xs_testxs, "tape")),
+      is.numeric(attr(xs_testxs, "depth")),
+      is.numeric(attr(xs_testxs, "baseline"))
 
+    )
+  )
 
   x
 }
 
-
-xs <- function(x, ...) {
-  UseMethod("xs", x)
-}
-
-xs.default <- function(x, ...){
-  new_xs(x = x, ...)
-}
-
-xs.data.frame <- function(xs,
-                          x_col,
-                          d_col,
-                          baseline) {
-  x_col <- deparse(substitute(x_col))
-  d_col <- deparse(substitute(d_col))
-  baseline <- deparse(substitute(basline))
-  xs <- new_xs(
-    xs,
-    tape = x_col,
-    depth = d_col,
-    baseline = baseline
-  )
-  return(xs)
-}
+#
+# xs <- function(x, ...) {
+#   UseMethod("xs", x)
+# }
+#
+# xs.default <- function(x, ...){
+#   new_xs(x = x, ...)
+# }
+#
+# xs.data.frame <- function(xs,
+#                           x_col,
+#                           d_col,
+#                           baseline) {
+#   x_col <- deparse(substitute(x_col))
+#   d_col <- deparse(substitute(d_col))
+#   baseline <- deparse(substitute(basline))
+#   xs <- new_xs(
+#     xs,
+#     tape = x_col,
+#     depth = d_col,
+#     baseline = baseline
+#   )
+#   return(xs)
+# }
--- R/xs_area.R
+++ R/xs_area.R
@@ -1,49 +1,42 @@
-#' @title Cross-Sectional area
-#'
-#' @description Calculate area of a cross-section using the trapezoidal rule for
-#'   integration. By default, uses the columns "TAPE" for measurement across xs,
-#'   "InvertRod" for depth relative to y = 0, and "Bankful" for baseline
-#'   measurement.
-#'
-#' @importFrom dplyr mutate summarize
-#' @importFrom rlang .data
-#' @param data A data.frame of cross-section data
-#' @param tape A column from \code{data} or a numeric vector of tape readings
-#'   ("x coordinates").
-#' @param depth A column from \code{data} or a numeric vector of rod readings
-#'   ("y coordinates").
-#' @param baseline A column from \code{data} or vector representing bankfull
-#'   level.
-#' @param sum_area A logical (default \code{TRUE}) indicating whether to return
-#'   a single summary of the trapezoidal area or a tibble indicating the area at
-#'   each section.
-#'
-#' @return A Double of the cross-sectional area compared to a baseline
+
 #' @export
-#'
-
-
-xs_area <- function(data, tape, depth, baseline, sum_area = TRUE){
-
-  # I know this is a horrible way of doing things, but I wanted to either use
-  # tidyeval (not quote column names) or pass a vector to one of the
-  # arguments.
-  A <- data %>%
-    dplyr::mutate(
-      depth_baseline = normalize_baseline({{depth}}, {{baseline}})
-      ) %>%
-    dplyr::summarize(
-      Area = trap_area({{tape}}, .data$depth_baseline)
-    ) %>%
-    `[[`(1)
-
-
-  if(sum_area) {
-    return(sum(A))
-  } else {
-    return(A)
-  }
+xs_area <- function(x) {
+  UseMethod("xs_area")
 }
+
+
+#' @export
+xs_area.default <- function(x, sum_area = TRUE) {
+  t <- attr(x, "tape")
+  d <- attr(x, "depth")
+  b <- attr(x, "baseline")
+
+  depth_baseline <- normalize_baseline(d, b)
+  Area <- trap_area(t, depth_baseline)
+  if(sum_area) return(sum(Area)) else return(Area)
+}
+
+# xs_area.data.frame <- function(data, tape, depth, baseline, sum_area = TRUE){
+#
+#   # I know this is a horrible way of doing things, but I wanted to either use
+#   # tidyeval (not quote column names) or pass a vector to one of the
+#   # arguments.
+#   A <- data %>%
+#     dplyr::mutate(
+#       depth_baseline = normalize_baseline({{depth}}, {{baseline}})
+#       ) %>%
+#     dplyr::summarize(
+#       Area = trap_area({{tape}}, .data$depth_baseline)
+#     ) %>%
+#     `[[`(1)
+#
+#
+#   if(sum_area) {
+#     return(sum(A))
+#   } else {
+#     return(A)
+#   }
+# }
 
 #' Normalize vector to baseline
 #' @param x Numeric vector
