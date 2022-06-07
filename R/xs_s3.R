0d866de1814793990a8ea128fcaa899dc29b3898
brownhr <brownhr314@gmail.com>
2022-06-06 22:07:36 -0400

    Update xs_s3.R

--- R/xs_s3.R
+++ R/xs_s3.R
@@ -1,25 +1,30 @@
-as_xs <- function(x){
+#' @export
+as_xs <- function(x) {
   UseMethod("as_xs")
 }
 
+
+#' @export
 as_xs.default <- function(x, tape, depth, baseline) {
   x <- structure(x,
-                 class = purrr::prepend(class(x), "xs"),
-                 tape = tape, depth = depth, baseline = baseline)
+    class = purrr::prepend(class(x), "xs"),
+    tape = tape, depth = depth, baseline = baseline
+  )
   return(x)
 }
 
 
 #' @export
 #' @method as_xs data.frame
+#' @importFrom rlang enquo
 
-as_xs.data.frame <- function(x, tape, depth, baseline){
+as_xs.data.frame <- function(x, tape, depth, baseline) {
   x <- structure(
     x,
     class = purrr::prepend(class(x), "xs"),
-    tape = x[[substitute(tape)]],
-    depth = x[[substitute(depth)]],
-    baseline = x[[substitute(baseline)]]
-
+    tape = x[[rlang::enquo(tape)]],
+    depth = x[[rlang::enquo(depth)]],
+    baseline = x[[rlang::enquo(baseline)]]
   )
+  return(x)
 }
